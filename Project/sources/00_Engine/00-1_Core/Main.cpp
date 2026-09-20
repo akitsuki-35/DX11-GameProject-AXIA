93827ecf8824d27dd0181ace4fdb2ac2a82b9672
あきつき <211625162+akitsuki -35@users.noreply.github.com>
2026-09-20 23:27:45 +0900

    refactor: アプリケーション初期化・終了関数定義

--- Project/sources/00_Engine/00-1_Core/Main.cpp
+++ Project/sources/00_Engine/00-1_Core/Main.cpp
@@ -11,46 +11,8 @@
 #include "SystemTimer.h"
 #include "AppLoop.h"
 
-#include "ShaderManager.h"
-#include "FontManager.h"
-#include "FontLoader.h"
-#include "Graphics.h"
-#include "Transition.h"
-#include "Input.h"
-#include "AudioPlayer.h"
-
-#include "Application.h"
-#include "Title.h"
-#include <memory>
+#include "AppSetup.h"
 #include <d3d11.h>
-
-// フォントロード
-// ゲーム起動時に一度だけ呼ぶ
-namespace FontSet {
-	inline void initialize() {
-		FontManager::getInstance().Register("Kaisotai", "assets\\fonts\\Kaisotai-Next-UP-B.ttf");
-	}
-}
-
-// シェーダー登録
-// ゲーム起動時に1度だけ呼ぶ
-namespace ShaderSet {
-	inline void initialize() {
-		ShaderManager::getInstance().Register("UI",
-			"assets\\shaders\\UISpriteVS.cso", "assets\\shaders\\UnlitPS.cso");
-
-		ShaderManager::getInstance().Register("Font",
-			"assets\\shaders\\UISpriteVS.cso", "assets\\shaders\\FontPS.cso");
-
-		ShaderManager::getInstance().Register("Unlit",
-			"assets\\shaders\\UnlitVS.cso", "assets\\shaders\\UnlitPS.cso");
-
-		ShaderManager::getInstance().Register("PBR",
-			"assets\\shaders\\PhongVS.cso", "assets\\shaders\\PBRPS.cso");
-	}
-}
-
-
 
 /*------------------------------------------------------------
 	メイン
@@ -67,19 +29,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	// ウィンドウ初期化
 	System::Window::getInstance().Initialize(hInstance);
 
-	// 各種初期化
-	D3D11::Graphics::getInstance().Initialize();
-
-	// シェーダー読み込み
-	ShaderSet::initialize();
-	FontSet::initialize();
-
-	Transition::getInstance().Initialize("assets\\textures\\white.png", "UI");
-	Input::Initialize();
-	AudioPlayer::InitializeMaster();
-
-	// 初期シーン設定
-	Application::getInstance().Initialize(std::make_unique<Title>());
+	AppSetup::Initialize();
 
 	// ウィンドウ表示
 	System::Window::getInstance().Show(nCmdShow);
@@ -92,11 +42,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 	timeEndPeriod(1);
 
-	Application::getInstance().Finalize();
-
-	AudioPlayer::FinalizeMaster();
-	Input::Finalize();
-	D3D11::Graphics::getInstance().Finalize();
+	AppSetup::Finalize();
 
 	return isQuit;
 }
